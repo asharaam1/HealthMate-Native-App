db0c2bfd6da6f17087380b910846808f3f95b706
asharaam1 <lohanaasharaam1@gmail.com>
2026-03-02 00:06:24 +0500

    Dependencies added first

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -6,4 +6,9 @@ export const COLORS = {
   white: '#FFFFFF',
   gray: '#666666',
   text: '#333333',
+  lightGray: '#F0F4F4',
+  textMain: '#1A2121',
+  textSub: '#526161',
+  error: '#EB5757',
+  inputBg: '#F1F3F5',
 };
